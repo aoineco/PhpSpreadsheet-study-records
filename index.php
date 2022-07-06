4e0bf6cfcd74f44ccb5964303c0a5a02529ff7b3
ai-aoi-sora <ai-aoi-sora@users.noreply.github.com>
2022-07-06 14:52:38 +0900

    files

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form method="post" action="import.php" enctype="multipart/form-data">
+    <div class="form-group">
+        <label for="exampleInputFile">File Upload</label>
+        <input type="file" name="file" class="form-control" id="exampleInputFile">
+    </div>
+    <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+</form>
+</body>
+</html>
